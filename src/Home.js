72565fb92e4a935724930364803fd8b15a3e0e86
Tabatha Slatton <valbatha@gmail.com>
2021-01-19 23:29:16 -0500

    added useEffect hook to home component to fetch data from a json server

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+// npx json-server --watch data/db.json --port 8000
+
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'Cracking the Coding Interview', body: "This book is really helpful and has shown me what an ever-evolving world coding is. I'm very inspired for the next steps of my journey.", author: 'Ken', id: 1 },
-        { title: 'How to Train your Puppy', body: 'Bark Bark Walking Bark Bark Fetch Bark Bark Treats..SOOO FETCH', author: 'Luma', id: 2 },
-        { title: 'Eloquent JavaScript', body: "Today I read chapter one. It was really great and I'm excited to continue to learn more about building my JS skills!", author: 'Scott', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
+    // takes in a function that will run every render
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(blogs => {
+                setBlogs(blogs)
+            })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            {/* <BlogList blogs={blogs.filter(blog => blog.author === "Ken")} title="Ken's Blogs"/> */}
+
         </div>
         );
 }
